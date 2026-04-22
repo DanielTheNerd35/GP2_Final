351baf5ef002d55ed4df04c5ef68a0d079cd701e
DanielTheNerd35 <vazquezd8@montclair.edu>
2026-04-21 22:52:25 -0400

    DeadState successful

--- Final_Project/Assets/Scripts/Enemy/Enemy_StateMachine/States/DeadState.cs
+++ Final_Project/Assets/Scripts/Enemy/Enemy_StateMachine/States/DeadState.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class DeadState : State
 {
-    protected override string AnimBoolName => "IsDamaged";
+    protected override string AnimBoolName => "IsDead";
 
     public DeadState(Enemy enemy) : base (enemy) {}
 
     public override void Enter()
     {
         base.Enter();
+        
     }
     
 }
